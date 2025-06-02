88226eaf9993b4e0f991f5c99d4cdba5589c168d
Choy050823 <choymh23@gmail.com>
2025-06-02 21:38:09 +0700

    edit aws s3 config

--- src/main/java/com/backend/wanderverse_server/config/S3Config.java
+++ src/main/java/com/backend/wanderverse_server/config/S3Config.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 
